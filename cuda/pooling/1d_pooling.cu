0ca66ff1b5e1c400e24768532bcd31731ac05628
romit <11757603+romitjain@users.noreply.github.com>
2025-04-08 17:26:02 +0530

    Sending 1/k ahead of time

--- cuda/pooling/1d_pooling.cu
+++ cuda/pooling/1d_pooling.cu
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <cuda_runtime.h>
 
+/*
+Optimizations
+1. Load the data required by block in SMEM in the start
+2. Send 1/k ahead of time
+3. Pragma unroll
+*/
+
 __global__ void pooling_1d(
     float *__restrict__ input,
     float *__restrict__ output,
@@ -8,21 +15,48 @@ __global__ void pooling_1d(
     int s,
     int p,
     size_t in_size,
-    size_t out_size)
+    size_t out_size,
+    float inv_k)
 {
-    int out_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (out_idx >= out_size) return;
+    // The strategy is to load the data from HBM to SMEM
+    // once for every block
+    extern __shared__ float sh_input[];
 
-    float accum = 0.0;
-    int in_idx = s*out_idx-p;
+    int block_start = blockIdx.x * blockDim.x;
+    int gmem_block_read_start = block_start * s - p;
+    int num_elements_to_load = s * (blockDim.x - 1) + k;
 
-    for (int i = in_idx; i < in_idx+k; i ++) {
-        if (i < in_size && i >= 0) {
-            accum += input[i];
+    int tidx = threadIdx.x;
+    for (int i = tidx; i < num_elements_to_load; i += blockDim.x)
+    {
+        int gmem_idx = gmem_block_read_start + i;
+        if (gmem_idx >= 0 && gmem_idx < in_size)
+        {
+            sh_input[i] = input[gmem_idx];
+        }
+        else
+        {
+            sh_input[i] = 0.0f;
         }
     }
 
-    output[out_idx] = accum/((float)(k));
+    __syncthreads();
+
+    int out_idx = block_start + tidx;
+    if (out_idx >= out_size) return;
+
+    float accum = 0.0;
+
+    int shmem_start_idx_for_thread = (s * out_idx - p) - gmem_block_read_start;
+
+#pragma unroll 8
+    for (int i = 0; i < k; ++i)
+    {
+        int current_sh_idx = shmem_start_idx_for_thread + i;
+        accum += sh_input[current_sh_idx];
+    }
+
+    output[out_idx] = accum * inv_k;
 }
 
 // Note: input, output are all device pointers to float32 arrays
@@ -44,15 +78,19 @@ extern "C" void solution(
     dim3 block_size(threads_x, 1, 1);
     dim3 grid(blocks_x, 1, 1);
 
-    pooling_1d<<<grid, block_size>>>(
+    int shmem_elems_per_block = stride * (threads_x - 1) + kernel_size;
+    size_t shmem_bytes = shmem_elems_per_block * sizeof(float);
+    float inv_k = 1.0f/(float)(kernel_size);
+
+    pooling_1d<<<grid, block_size, shmem_bytes>>>(
         d_input,
         d_output,
         kernel_size,
         stride,
         padding,
         H,
-        output_size
-    );
+        output_size,
+        inv_k);
 
     cudaError_t error = cudaGetLastError();
     if (error != cudaSuccess) {
